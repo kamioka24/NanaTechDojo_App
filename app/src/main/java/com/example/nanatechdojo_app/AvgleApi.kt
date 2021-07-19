5176f788cf2bec33c0ca79effe256619c63004a5
kamioka24 <keee2404@gmail.com>
2021-07-19 21:55:55 +0900

    [Break]

--- app/src/main/java/com/example/nanatechdojo_app/AvgleApi.kt
+++ app/src/main/java/com/example/nanatechdojo_app/AvgleApi.kt
@@ -4,15 +4,16 @@ import com.example.nanatechdojo_app.model.AvCategory
 import com.example.nanatechdojo_app.model.AvCollection
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface AvgleApi {
     companion object {
         const val BASE_URL = "https://api.avgle.com/v1/"
     }
     @GET("categories")
-    // API 通信は必ず suspend
-    suspend fun getAvCategories(): Response<AvCategory>
+    // コルーチンの中でのみ呼び出すことができる suspend (非同期処理)
+    suspend fun getAvCategories(): AvCategory
 
     @GET("collections/1")
-    suspend fun getAvCollections(): Response<AvCollection>
+    suspend fun getAvCollections(): AvCollection
 }
--- app/src/main/java/com/example/nanatechdojo_app/main/MainActivity.kt
+++ app/src/main/java/com/example/nanatechdojo_app/main/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.nanatechdojo_app.main
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nanatechdojo_app.R
 import com.example.nanatechdojo_app.repository.AvRepository
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -18,22 +16,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            tappedCategoryButton()
+        val categoriesButton = findViewById<Button>(R.id.categories_button)
+        categoriesButton.setOnClickListener {
+            tappedCategorycategories_button()
+        }
+
+        val collectionsButton = findViewById<Button>(R.id.collections_button)
+        collectionsButton.setOnClickListener {
+            tappedCollectioncollections_button()
         }
     }
 
-    fun tappedCategoryButton() {
+    fun tappedCategorycategories_button() {
         val service = AvRepository().getRetrofit()
         val scope = CoroutineScope(Dispatchers.IO)
         scope.launch {
             try {
                 val avCategories = service.getAvCategories()
-                val avCollections = service.getAvCollections()
                 val text1 = findViewById<TextView>(R.id.textView1)
-                val text2 = findViewById<TextView>(R.id.textView2)
                 text1.text = avCategories.toString()
+            } catch(e:Exception) {
+                print("はずれ")
+            }
+        }
+    }
+
+    fun tappedCollectioncollections_button() {
+        val service = AvRepository().getRetrofit()
+        val scope = CoroutineScope(Dispatchers.IO)
+        scope.launch {
+            try {
+                val avCollections = service.getAvCollections()
+                val text2 = findViewById<TextView>(R.id.textView2)
                 text2.text = avCollections.toString()
             } catch(e:Exception) {
                 print("はずれ")
